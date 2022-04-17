f200591ff6d7dec1e0b12caf47eeaf9c7a048e98
TylerLott <tylerlott0126@gmail.com>
2022-04-17 04:28:13 -0600

    homepage

--- client-server/index.js
+++ client-server/index.js
@@ -2,18 +2,20 @@ const express = require("express")
 
 const app = express()
 
+let PRE_PATH = "/daddyscummies"
 let PATH = "/build"
 let PATH_2 = "/build/index.html"
 if (process.env.NODE_ENV !== "production") {
   PATH = "D:/Projects/fn-mondays/user/client/build"
   PATH_2 = "D:/Projects/fn-mondays/user/client/build/index.html"
+  PRE_PATH = ""
 }
 
 // Serve the static files from the React app
-app.use(express.static(PATH))
+app.use(PRE_PATH, express.static(PATH))
 
 // Handles any requests that don't match the ones above
-app.get("/daddyscummies*", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(PATH_2)
 })
 
